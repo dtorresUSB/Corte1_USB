662316918531e29ab88ac578e9393da00712e80f
dtorresUSB <dtorres@academia.usbbog.edu.co>
2023-02-16 18:39:03 -0500

    programa cuadratica

---
+++ Cuadratica.py
@@ -0,0 +1,11 @@
+from math import sqrt as raizc
+
+a = eval(input('Ingrese el valor de a = '))
+b = int(input('Ingrese el valor de b = '))
+c = int(input('Ingrese el valor de c = '))
+
+x1 = (-b + raizc((b**2)-4*a*c))/(2*a)
+x2 = (-b - raizc((b**2)-4*a*c))/(2*a)
+
+print('el primer resultado es ', x1)
+print('el segundo resultado es ', x2)
