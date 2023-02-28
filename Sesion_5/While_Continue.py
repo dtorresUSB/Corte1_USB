f50eb8b489616881134d10b8cb8bd8c996ad3de0
dtorresUSB <dtorres@academia.usbbog.edu.co>
2023-02-28 15:32:46 -0500

    Sesion 5 V2

---
+++ Sesion_5/While_Continue.py
@@ -0,0 +1,11 @@
+n=0
+while n<10:
+    if n==6:
+        #print(n)
+        n+=1
+        continue
+    print(n)
+    n+=1            #Contador n=n+1
+print('fin del proceso')
+
+#En el caso de queda en un ciclo infinito oprimir Ctrl+C
