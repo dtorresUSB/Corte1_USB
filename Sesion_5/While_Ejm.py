846f2d7aa6e9310fd834ab5081a6fca4be7bc54c
dtorresUSB <dtorres@academia.usbbog.edu.co>
2023-02-28 19:12:59 -0500

    Ejercicio While

---
+++ Sesion_5/While_Ejm.py
@@ -0,0 +1,12 @@
+
+var = 'si'
+while var=='si':
+    var=input('¿sigue saltando? ')
+    print('saltando')
+
+    if var == 'no':
+        break
+    else:
+        var = 'si'
+
+print('termine de saltar')
